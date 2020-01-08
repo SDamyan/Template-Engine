90131225eb57df68063ee5713c5fb60dcc10d635
Sara Elin <sedamyan@gmail.com>
2020-01-08 14:58:38 -0600

    added npm i

---
+++ server.js
@@ -0,0 +1,10 @@
+var inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+        //pass questions in here
+    ])
+    .then(answers => {
+        //use user feedback for whatever you need
+    });
+
